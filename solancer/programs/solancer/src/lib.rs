cc74ae55979df879417950b810b38188be77e0a5
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-05-24 21:43:07 +0700

    WIP: create user account

--- solancer/programs/solancer/src/lib.rs
+++ solancer/programs/solancer/src/lib.rs
@@ -1,15 +1,81 @@
 use anchor_lang::prelude::*;
-
+use anchor_spl::token::{self, Token};
+use std::mem::size_of;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+const USER_NAME_LENGTH: usize = 100;
+const USER_URL_LENGTH: usize = 255;
+const USER_ROLE_LENGTH: usize = 10;
 #[program]
 pub mod solancer {
+    use anchor_lang::solana_program::log::sol_log_compute_units;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    /// Create user
+    /// @param name:        user name
+    /// @param profile_url: user profile url
+    pub fn create_user(
+        ctx: Context<CreateUser>,
+        name: String,
+        profile_url: String,
+        role: String
+    ) -> anchor_lang::Result<()> {
+        if name.trim().is_empty() || profile_url.trim().is_empty() {
+            return Err(error!(Errors::CannotCreateUser));
+        }
+        let user = &mut ctx.accounts.user;
+        user.wallet_address = ctx.accounts.authority.key();
+        user.name = name;
+        user.profile_image_url = profile_url;
+        user.role = role;
+
+        msg!("User Added!"); //logging
+        sol_log_compute_units(); //Logs how many compute units are left, important for budget
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateUser<'info> {
+    // Authenticate user account
+    #[account(
+        init,
+        // User account use string "user" and index of user as seeds
+        seeds = [b"user".as_ref(), authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = size_of::<UserAccount>() + USER_NAME_LENGTH + USER_URL_LENGTH + USER_ROLE_LENGTH + 8
+    )]
+    pub user: Account<'info, UserAccount>,
+
+    // Authority (this is signer who paid transaction fee)
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    /// System program
+    /// CHECK: Simple test account
+    pub system_program: UncheckedAccount<'info>,
+
+    // Token program
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    // Clock to save time
+    pub clock: Sysvar<'info, Clock>,
+}
+
+#[account]
+pub struct UserAccount {
+    pub wallet_address: Pubkey,
+    pub name: String,
+    pub profile_image_url: String,
+    pub role: String
+}
+
+#[error_code]
+pub enum Errors {
+    #[msg("User cannot be created, missing data")]
+    CannotCreateUser,
+
+}
