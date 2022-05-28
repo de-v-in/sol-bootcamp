c8d251adc2fc3ed7d191df21f965fbdfda5e5df9
Tran Cong Duy Nguyen <tcdnguyen1997@gmail.com>
2022-05-28 22:30:40 +0700

    WIP: Implement pegging for developer/company

--- solancer/programs/solancer/src/instructions.rs
+++ solancer/programs/solancer/src/instructions.rs
@@ -149,3 +149,77 @@ pub struct UpdateInterviewResult<'info> {
     #[account(mut)]
     pub authority: Signer<'info>,
 }
+
+#[derive(Accounts)]
+pub struct CreateContract<'info> {
+    #[account(
+        init,
+        seeds = [b"contract".as_ref(), authority.key().as_ref()],
+        bump,
+        payer = authority,
+        space = ContractAccount::MAX_SIZE + 8
+    )]
+    pub contract: Account<'info, ContractAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    #[account(seeds = [b"treasurer".as_ref(), contract.company.as_ref(), contract.developer.as_ref()], bump)]
+    /// CHECK: Just a pure account
+    pub treasurer: AccountInfo<'info>,
+
+    #[account(
+        init,
+        payer = authority,
+        associated_token::mint = mint,
+        associated_token::authority = treasurer
+        )]
+    pub token_account: Account<'info, token::TokenAccount>,
+
+    /// CHECK: Simple test account
+    pub system_program: Program<'info, System>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub mint: Box<Account<'info, token::Mint>>,
+    pub clock: Sysvar<'info, Clock>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct PegContract<'info> {
+    #[account(mut)]
+    pub contract: Account<'info, ContractAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(constraint = token_program.key == &token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    #[account(seeds = [b"treasurer".as_ref(), contract.company.as_ref(), contract.developer.as_ref()], bump)]
+    /// CHECK: Just a pure account
+    pub treasurer: AccountInfo<'info>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = treasurer
+        )]
+    pub contract_token_account: Account<'info, token::TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = authority
+        )]
+    pub owner_token_account: Account<'info, token::TokenAccount>,
+
+    /// CHECK: Simple test account
+    pub system_program: Program<'info, System>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub mint: Box<Account<'info, token::Mint>>,
+}
